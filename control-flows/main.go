a49c0c1838fb06cdf2d1c4943780e1964cb876d0
LAChapman <lukechappman@gmail.com>
2026-07-02 21:15:51 +0100

    Added second program for if statements and switches

---
+++ control-flows/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	type score struct {
+		name  string
+		score int
+	}
+
+	scores := []score{
+		{name: "Chapman, Luke", score: 87},
+		{name: "Smith, John", score: 85},
+		{name: "Kettle, James", score: 99},
+	}
+
+	fmt.Println("Select score to print (1 - 3):")
+	var option string   // delcare our variable
+	fmt.Scanln(&option) // takes user input from CLI, points to memory location of "option"
+
+	fmt.Println("Student Scores")
+	fmt.Println(strings.Repeat("-", 17))
+	var index int
+	if option == "1" { // use strconv package if using on something like a website
+		index = 0
+	} else if option == "2" {
+		index = 1
+	} else if option == "3" {
+		index = 2
+	} else {
+		fmt.Println("Unknown Option, defaulting to 1")
+		index = 0
+	}
+	fmt.Println(scores[index].name, scores[index].score)
+}
--- first-program/main.go
+++ first-program/main.go
@@ -9,9 +9,28 @@ func main() {
 	// name := "Chapman, Luke"
 	// score := 90
 
-	name, score := "Chapman, Luke", 90
+	type score struct {
+		name  string
+		score int
+	}
+
+	// students := []string{
+	// 	"Chapman, Luke",
+	// 	"Smith, John",
+	// 	"Kettle, James", // notice the trailing comma at the end of the array, for strings we need this
+	// }
+	scores := []score{
+		{name: "Chapman, Luke", score: 87},
+		{name: "Smith, John", score: 85},
+		{name: "Kettle, James", score: 99},
+	}
+
+	// score := []int{88, 83, 79}
+	// name, score := "Chapman, Luke", 90
 
 	fmt.Println("Student Scores")
 	fmt.Println(strings.Repeat("-", 17))
-	fmt.Println(name, score)
+	fmt.Println(scores[0].name, scores[0].score)
+	fmt.Println(scores[1].name, scores[1].score)
+	fmt.Println(scores[2].name, scores[2].score)
 }
